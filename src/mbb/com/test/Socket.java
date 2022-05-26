e06a307153ab2af4f8e53c4b3ca806a57d1f3bc9
Alex Zotov <azotov@hotmail.com>
2022-05-26 18:21:08 +0800

    Update Socket.java

--- src/mbb/com/test/Socket.java
+++ src/mbb/com/test/Socket.java
@@ -7,31 +7,8 @@ public class Socket {
 	}
 	
 	public void plugIn(Device device) {
-		System.out.println("You have plugged in a "+device.getName())
-		if (device.equals("Sony Playstation 4")) {
-			System.out.println("You have plugged in a Sony Playstation 4");
-		} 
-		else if (device.equals("Microsoft XBox One")) {
-			System.out.println("You have plugged in a Microsoft XBox One");
-		}
-		else if (device.equals("Sony Smart TV")) {
-			System.out.println("You have plugged in a Sony Smart TV");
-		}
-		else if (device.equals("Apple Macbook")) {
-			System.out.println("You have plugged in an Apple Macbook");
-		}
-		else if (device.equals("Samsung Smart TV")) {
-			System.out.println("You have plugged in a Samsung Smart TV");
-		}
-		else if (device.equals("Samsung Vacuum Cleaner")) {
-			System.out.println("You have plugged in a Samsung Vacuum Cleaner");
-		}
-		else if (device.equals("Samsung Smart TV")) {
-			System.out.println("You have plugged in a Samsung Laserjet Printer");
-		} else {
-			System.out.println("You have plugged in a Generic Electrical Device");
-		}
-
+		System.out.println("You have plugged in a "+device.getName());
+		plugIns.append(device);
 	}
 	
 	
